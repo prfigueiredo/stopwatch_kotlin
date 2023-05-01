2ee25258b4758d3e91bb7790c52dcaaecd23957b
prfigueiredo <paulofigueiredo@id.uff.br>
2023-05-01 18:55:09 -0300

    Implementing button to start the timer

--- app/src/main/java/com/example/stopwatch_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch_kotlin/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.stopwatch_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.example.stopwatch_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var running = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(getLayoutInflater())
+        setContentView(binding.getRoot())
+
+        binding.iniciar.setOnClickListener {
+            IniciarCronometro()
+        }
+    }
+    private fun IniciarCronometro(){
+        if(!running){
+            binding.cronometro.base = SystemClock.elapsedRealtime()
+            binding.cronometro.start()
+            running = true
+        }
     }
 }
